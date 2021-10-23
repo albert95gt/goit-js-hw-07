fe34fdc57d4ba114cb60b479bb1937352689e8d3
Albert Takács <albitakacs@gmail.com>
2021-10-23 20:01:54 +0200

    finish task 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryImageMarkup = createGalleryItems();
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryImageMarkup);
+
+galleryContainer.addEventListener("click", onGalleryImage); 
+
+
+function createGalleryItems () {
+    return galleryItems.map(({preview, original}) => {
+        return `<a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="Image description"
+        />
+        </a>`
+    }).join("");
+}
+
+let instance;
+
+function onGalleryImage(event) {
+    event.preventDefault();
+    
+    if(event.target.nodeName !== "IMG"){
+        return;
+    }
+    const galleryLargeImgSrc = event.target.dataset.source;  
+
+    instance = basicLightbox.create(`<img src="${galleryLargeImgSrc}">`);
+    instance.show(() => galleryContainer.addEventListener("keydown", closeGalleryImage));
+    
+
+    
+}
+
+function closeGalleryImage(event){
+
+    if(event.code === "Escape"){
+        instance.close(() => {
+            removeEventListenerForGalleryContainer();
+
+        });
+    }
+}
+
+function removeEventListenerForGalleryContainer () {
+    galleryContainer.removeEventListener("keydown", closeGalleryImage)
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsList = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItems(galleryItems);
+
+galleryItemsList.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+function createGalleryItems(items) {
+    return galleryItems.map(({preview, original, description}) => {
+        
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+    }).join(""); 
+}
+
+let gallery = new SimpleLightbox('.gallery__item', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+galleryItemsList.addEventListener("click", onClickGalleryImg);
+
+function onClickGalleryImg (event) {
+    event.preventDefault();
+
+    if(!event.target.classList.contains("gallery__image")){
+        return;
+    }
+    gallery.on('show.simplelightbox');
+    
+}
+
+
+
+    
+
